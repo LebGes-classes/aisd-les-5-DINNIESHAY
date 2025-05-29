a86975746e0fc48f7c4e0b463f1b6e76b8f80878
DINNIESHAY <din200510@gmail.com>
2025-05-30 02:22:32 +0300

    Реализована неубывающая очередь с приоритетами на основе бинарной пирамиды

---
+++ Queue/Queue/PriorityQueue.cpp
@@ -0,0 +1,72 @@
+﻿#include "PriorityQueue.h"
+
+void PriorityQueue::enqueue(int priority, const std::string & data)
+{
+  Item item = Item(priority, data);
+  heap.push(item);
+}
+
+std::string PriorityQueue::dequeue()
+{
+  Item itemToDel = heap.pop();
+
+  return itemToDel.data;
+}
+
+std::string PriorityQueue::peek()
+{
+  Item minItem = heap.getMin();
+
+  return minItem.data;
+}
+
+bool PriorityQueue::isEmpty()
+{
+  bool isEmpty;
+
+  if (heap.getSize() != 0)
+  {
+    isEmpty = false;
+  }
+  else
+  {
+    isEmpty = true;
+  }
+
+  return isEmpty;
+}
+
+void PriorityQueue::print()
+{
+  while (heap.getSize() != 0)
+  {
+    std::cout << dequeue() << " ";
+  }
+}
+
+PriorityQueue::Item::Item(int priority, std::string data)
+{
+  this->priority = priority;
+  this->data = data;
+}
+
+bool PriorityQueue::Item::operator<(const Item & otherItem) const
+{
+  return this->priority < otherItem.priority;
+}
+
+bool PriorityQueue::Item::operator<=(const Item & otherItem) const
+{
+  return this->priority <= otherItem.priority;
+}
+
+bool PriorityQueue::Item::operator>(const Item & otherItem) const
+{
+  return this->priority > otherItem.priority;
+}
+
+bool PriorityQueue::Item::operator>=(const Item & otherItem) const
+{
+  return this->priority >= otherItem.priority;
+}
+
---
+++ Queue/Queue/PriorityQueue.h
@@ -0,0 +1,56 @@
+﻿#ifndef PRIORITY_QUEUE_H
+#define PRIORITY_QUEUE_H
+
+#include <string>
+#include <vector>
+#include <utility>
+#include <iostream>
+
+#include "MinBinaryHeap.h"
+
+/*Неубывающая очередь с приоритетами на основе бинарной пирамиды.
+  Элементы с меньшими значениями имеют более высокий приоритет*/
+class PriorityQueue
+{
+public:
+
+  //Добавление элемента в очередь
+  void enqueue(int priority, const std::string & data);
+
+  //Извлечение элемента с наивысшим приоритетом из очереди
+  std::string dequeue();
+
+  //Получение элемента с наивысшим приоритетом
+  std::string peek();
+
+  //Проверка на пустоту очереди
+  bool isEmpty();
+
+  //Вывод элементов очереди
+  void print();
+
+private:
+
+  //Item - элемент очереди, который хранит в себе приоритет и данные
+  struct Item
+  {
+    int priority;
+    std::string data;
+
+    Item(int priority, std::string data);
+
+    //Перегрузка операторов <, <=, >, >=
+    bool operator<(const Item & otherItem) const;
+
+    bool operator<=(const Item & otherItem) const;
+
+    bool operator>(const Item & otherItem) const;
+
+    bool operator>=(const Item & otherItem) const;
+  };
+
+  //Минимальная бинарная пирамида
+   MinBinaryHeap<Item> heap;
+};
+
+#endif
