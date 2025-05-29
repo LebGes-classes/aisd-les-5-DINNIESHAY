fe15571542d6b5ead6ecd9136dad683d3d74890e
DINNIESHAY <din200510@gmail.com>
2025-05-30 02:50:04 +0300

    В main добавлено тестирование удаления элементов

--- Queue/Queue/Main.cpp
+++ Queue/Queue/Main.cpp
@@ -14,7 +14,16 @@ int main()
   queue.enqueue(9, "f");
   queue.enqueue(14, "g");
   queue.enqueue(13, "h");
-
+  std::cout << "Результат до удаления элементов: ";
   queue.print();
-  //Результат: b a f d c h e g
+
+  queue.dequeue();
+  queue.dequeue();
+  std::cout << "Результат после удаления элементов: ";
+  queue.print();
+
+  /*
+    Результат до удаления элементов: b a f d c h e g
+    Результат после удаления элементов: f d c h e g
+  */
 }
